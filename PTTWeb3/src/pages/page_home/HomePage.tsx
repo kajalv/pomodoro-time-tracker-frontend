c974b9818bb0eaa902fda51a955bba0d7aba7dc6
Kajal Varma <kajalv@gatech.edu>
2019-03-13 04:57:56 -0400

    Add some look to homepage

--- PTTWeb3/src/pages/page_home/HomePage.tsx
+++ PTTWeb3/src/pages/page_home/HomePage.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {List, ListItem, Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox, Backdrop} from '@material-ui/core';
 
 
 class HomePage extends React.Component {
 
   render() {
+    var classes = {
+      homePage: "homePage",
+      loginLink: "loginLink"
+    }
+
     return (
-      <div>
-        <h2> Home </h2>
-        <Link to='/login'>LoginPage</Link>
+      <div className={classes.homePage}>
+        <img src={require("../../assets/large_pomodoro.png")}></img>
+        <Link to='/login' style={{ textDecoration: 'none' }}>
+          <div className={classes.loginLink}>
+            <Button variant="contained" style={{ textTransform: "none" }}>
+              Get Started!
+            </Button>
+          </div>
+        </Link>
       </div>
     );
   }
